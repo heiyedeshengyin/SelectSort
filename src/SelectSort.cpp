779859d9246ab55f7f66f27d4106323b24906f66
heiyedeshengyin <42495512+heiyedeshengyin@users.noreply.github.com>
2018-11-22 15:57:06 +0800

    Update SelectSort.cpp

--- src/SelectSort.cpp
+++ src/SelectSort.cpp
@@ -22,18 +22,12 @@ int main()
 
 void SelectSort(int *p, int length)
 {
-	int i, j, k, t;
-	for (i = 0; i < length; i++)				//选择排序(降序)
-	{
-		k = i;						//k为当前最大数下标，初始值设为i
-		for (j = i + 1; j < length; j++)
-			if (*(p + k) < *(p + j))		//存在比a[k]大的数a[j]
-				k = j;				//更改最大数的下标值
-		if (k != i)					//如果最大数的下标有更改，将最大数a[k]与a[i]交换
-		{
-			t = *(p + i);
-			*(p + i) = *(p + k);
-			*(p + k) = t;
-		}
-	}
+	for (int i = 0; i < length - 1; i++)
+		for (int j = i + 1; j < length; j++)
+			if (*(p + i) > *(p + j))
+			{
+				int temp = *(p + i);
+				*(p + i) = *(p + j);
+				*(p + j) = temp;
+			}
 }
